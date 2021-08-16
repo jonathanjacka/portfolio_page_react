337e0b9631ec85f516e8fc1e92514d5205e8f742
Jonathan Jacka <jonathanjacka@gmail.com>
2021-08-16 16:52:53 -0500

    Updated styling and function of navBar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
 
 function App() {
-  return <h1>Hello world!</h1>;
+  return <Navbar />;
 }
 
 export default App;
