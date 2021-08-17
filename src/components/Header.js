034df2683a06da623950c44b4b0fb889f77c349e
Jonathan Jacka <jonathanjacka@gmail.com>
2021-08-17 16:04:42 -0500

    Updated header image and responsive effects

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,20 +11,45 @@ function Header() {
           params={{
             particles: {
               number: {
-                value: 50,
+                value: 60,
+                density: {
+                  enable: true,
+                  value_area: 1500,
+                },
+              },
+              line_linked: {
+                enable: true,
+                opacity: 0.02,
+              },
+              move: {
+                direction: 'right',
+                speed: 0.05,
               },
               size: {
-                value: 3,
+                value: 1,
+              },
+              opacity: {
+                anim: {
+                  enable: true,
+                  speed: 1,
+                  opacity_min: 0.05,
+                },
               },
             },
             interactivity: {
               events: {
-                onhover: {
+                onclick: {
                   enable: true,
-                  mode: 'repulse',
+                  mode: 'push',
+                },
+              },
+              modes: {
+                push: {
+                  particles_nb: 1,
                 },
               },
             },
+            retina_detect: true,
           }}
         />
         <FadeIn delay={700} transitionDuration={1000}>
