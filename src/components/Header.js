d1e2355ba1eff7424e11b30d16ab4170189634bf
Jonathan Jacka <jonathanjacka@gmail.com>
2021-08-17 15:45:15 -0500

    Updated header with main info and particles animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import FadeIn from 'react-fade-in';
+import Particles from 'react-particles-js';
 
 function Header() {
   return (
     <div className='header-wrapper'>
       <div className='header-main-info'>
+        <Particles
+          className='header-main-info-particles'
+          params={{
+            particles: {
+              number: {
+                value: 50,
+              },
+              size: {
+                value: 3,
+              },
+            },
+            interactivity: {
+              events: {
+                onhover: {
+                  enable: true,
+                  mode: 'repulse',
+                },
+              },
+            },
+          }}
+        />
         <FadeIn delay={700} transitionDuration={1000}>
-          <h1>I am a teacher</h1>
+          <h1>I am a teacher.</h1>
         </FadeIn>
       </div>
     </div>
