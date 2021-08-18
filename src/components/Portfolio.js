95931d643253e2de4b25c8e76f6da0b62e99be1d
Jonathan Jacka <jonathanjacka@gmail.com>
2021-08-18 16:50:10 -0500

    Added basic component with images

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -9,30 +9,43 @@ export const Portfolio = () => {
   return (
     <div className='portfolio-wrapper'>
       <div className='container'>
-        <h1 className='text-uppercase text-center py-5'>Portfolio</h1>
-        {/* -- */}
-        <div className='image-box-wrapper ow justify-content-center'>
-          <img className='portfolio-image' src={resume} alt='resume' />
-        </div>
-        {/* -- */}
-        <div className='image-box-wrapper ow justify-content-center'>
-          <img
-            className='portfolio-image'
-            src={gameDesign}
-            alt='game design elective'
-          />
-        </div>
-        {/* -- */}
-        <div className='image-box-wrapper ow justify-content-center'>
-          <img
-            className='portfolio-image'
-            src={flippedClassroom}
-            alt='flipped classroom presentation'
-          />
-        </div>
-        {/* -- */}
-        <div className='image-box-wrapper ow justify-content-center'>
-          <img className='portfolio-image' src={portfolio} alt='portfolio' />
+        <h1 className='text-center py-5'>Some things I've done</h1>
+
+        <div className='image-box-wrapper'>
+          {/* -- */}
+
+          <div className='portfolio-image-box'>
+            <img className='portfolio-image' src={resume} alt='resume' />
+            <h3>My resume</h3>
+          </div>
+
+          {/* -- */}
+
+          <div className='portfolio-image-box'>
+            <img
+              className='portfolio-image'
+              src={gameDesign}
+              alt='game design elective'
+            />
+          </div>
+
+          {/* -- */}
+
+          <div className='portfolio-image-box'>
+            <img
+              className='portfolio-image'
+              src={flippedClassroom}
+              alt='flipped classroom presentation'
+            />
+          </div>
+
+          {/* -- */}
+
+          <div className='portfolio-image-box'>
+            <img className='portfolio-image' src={portfolio} alt='portfolio' />
+          </div>
+
+          {/* -- */}
         </div>
       </div>
     </div>
