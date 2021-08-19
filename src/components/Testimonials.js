4ebfbe016ed9597e1e8ed85e11f066c3d86fd88e
Jonathan Jacka <jonathanjacka@gmail.com>
2021-08-19 06:12:53 -0500

    Imported and begun building testimonial carousel

---
+++ src/components/Testimonials.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Testimonials = () => {
+  return (
+    <div className='testimonials'>
+      <h1>What my students say</h1>
+      <div className='container'>
+        <div className='testimonials-content'></div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/TestimonialsCarousel.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
+
+import avatar1 from '../assets/avatars/avatar-1.png';
+import avatar2 from '../assets/avatars/avatar-2.png';
+import avatar3 from '../assets/avatars/avatar-3.png';
+import avatar4 from '../assets/avatars/avatar-4.png';
+import avatar5 from '../assets/avatars/avatar-5.png';
+
+export const TestimonialsCarousel = () => {
+  return (
+    <Carousel>
+      <></>
+    </Carousel>
+  );
+};
