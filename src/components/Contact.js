8d47c73281f607512c74817614f053398e8b52a6
Jonathan Jacka <jonathanjacka@gmail.com>
2021-08-19 16:25:48 -0500

    Added email form send response functionality

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import emailjs from 'emailjs-com';
+
+import { service_id, template_id, user_id } from '../keys/keys';
 
 export const Contact = () => {
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(service_id, template_id, e.target, user_id).then(
+      (result) => {
+        console.log(result.text);
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
+  };
+
   return (
     <div className='contact-wrapper'>
       <div className='contact-form-container'>
@@ -14,65 +30,67 @@ export const Contact = () => {
         </div>
 
         <div className='container'>
-          <div className='row'>
-            <div className='col-md-6 col-xs-12'>
-              {/* NAME */}
-              <input
-                className='form-control'
-                id='name-input'
-                placeholder='Name'
-                name='name'
-                type='text'
-                required
-              />
-              {/* PHONE */}
-              <input
-                id='phone-input'
-                className='form-control'
-                placeholder='Contact Number'
-                name='phone'
-                type='text'
-              />
-              {/* EMAIL */}
-              <input
-                id='email-input'
-                className='form-control'
-                placeholder='Email'
-                name='email'
-                type='email'
-                required
-              />
-              {/* SUBJECT */}
-              <input
-                id='subject-input'
-                className='form-control'
-                placeholder='Subject'
-                name='subject'
-                type='text'
-                required
-              />
-            </div>
+          <form onSubmit={sendEmail}>
+            <div className='row'>
+              <div className='col-md-6 col-xs-12'>
+                {/* NAME */}
+                <input
+                  className='form-control'
+                  id='name-input'
+                  placeholder='Name'
+                  name='name'
+                  type='text'
+                  required
+                />
+                {/* PHONE */}
+                <input
+                  id='phone-input'
+                  className='form-control'
+                  placeholder='Contact Number'
+                  name='phone'
+                  type='text'
+                />
+                {/* EMAIL */}
+                <input
+                  id='email-input'
+                  className='form-control'
+                  placeholder='Email'
+                  name='email'
+                  type='email'
+                  required
+                />
+                {/* SUBJECT */}
+                <input
+                  id='subject-input'
+                  className='form-control'
+                  placeholder='Subject'
+                  name='subject'
+                  type='text'
+                  required
+                />
+              </div>
 
-            <div className='col-md-6 col-xs-12'>
-              {/* TEXT AREA / MESSAGE */}
-              <textarea
-                type='text'
-                className='form-control'
-                id='message-input'
-                name='message'
-                placeholder='Message'
-                required
-              ></textarea>
+              <div className='col-md-6 col-xs-12'>
+                {/* TEXT AREA / MESSAGE */}
+                <textarea
+                  type='text'
+                  className='form-control'
+                  id='message-input'
+                  name='message'
+                  placeholder='Message'
+                  required
+                ></textarea>
+              </div>
+              <button
+                className='btn-main-offer contact-btn'
+                type='submit'
+                id='contact-form-btn'
+              >
+                <span></span>
+                Send Message
+              </button>
             </div>
-            <button
-              className='btn-main-offer contact-btn'
-              type='submit'
-              id='contact-form-btn'
-            >
-              <span></span>
-              Send Message
-            </button>
-          </div>
+          </form>
         </div>
       </div>
     </div>
