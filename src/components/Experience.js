61abc35c80a862bb23879156db066dcee9dd1c2c
Jonathan Jacka <jonathanjacka@gmail.com>
2021-08-20 15:46:09 -0500

    Updated content of Experience Component

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -12,11 +12,17 @@ export const Experience = () => {
         <div className='timeline-block timeline-block-right'>
           <div className='marker'></div>
           <div className='timeline-content'>
-            <h3>2012-2014</h3>
+            <h3>2016-2019: Solorio Academy High School</h3>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hic nihil
-              fuit, quod quaereremus. At Zeno eum non beatum modo, sed etiam
-              divitem dicere ausus est. Duo Reges: constructio interrete.
+              Aside from teaching both Computer Science and English Literature,
+              I was part of the Professional Development Leadership Team where
+              we planned, organized and executed a two-year-long professional
+              development program for teaching staff in line with school- and
+              district-wide STEM performance goals.
+            </p>
+            <p>
+              I was also one of the founding members of the school's CS
+              department.
             </p>
           </div>
         </div>
@@ -25,11 +31,13 @@ export const Experience = () => {
         <div className='timeline-block timeline-block-left'>
           <div className='marker'></div>
           <div className='timeline-content'>
-            <h3>2014-2016</h3>
+            <h3>2019-2021: Walter Payton College Prep</h3>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hic nihil
-              fuit, quod quaereremus. At Zeno eum non beatum modo, sed etiam
-              divitem dicere ausus est. Duo Reges: constructio interrete.
+              I taught both Exploring Computer Science and AP Computer Science
+              Principles, and introduced the school's first AP Computer Science
+              A class in 2020. Furthermore, I was the head coach of the school's
+              Robotics Team (FirstRobotics - FTC), which qualified for the State
+              Championships in 2021.
             </p>
           </div>
         </div>
@@ -38,24 +46,11 @@ export const Experience = () => {
         <div className='timeline-block timeline-block-right'>
           <div className='marker'></div>
           <div className='timeline-content'>
-            <h3>2016-2018</h3>
+            <h3>2021-Present: Lane Tech College Prep</h3>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hic nihil
-              fuit, quod quaereremus. At Zeno eum non beatum modo, sed etiam
-              divitem dicere ausus est. Duo Reges: constructio interrete.
-            </p>
-          </div>
-        </div>
-
-        {/* -- */}
-        <div className='timeline-block timeline-block-left'>
-          <div className='marker'></div>
-          <div className='timeline-content'>
-            <h3>2018-2021</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Hic nihil
-              fuit, quod quaereremus. At Zeno eum non beatum modo, sed etiam
-              divitem dicere ausus est. Duo Reges: constructio interrete.
+              I joined one of the most prominent high school Computer Science
+              departments in the country, where I currently teach Exploring
+              Computer Science and Programming: Media and Computation.
             </p>
           </div>
         </div>
