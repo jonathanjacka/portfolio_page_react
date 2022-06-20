1a06bd42b3ed255722d8223aa893b1e075cb4c1d
Jonathan Jacka <jonathanjacka@gmail.com>
2022-06-20 12:19:46 -0500

    Updated Portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import MediaCard from './MediaCard';
 
-import flippedClassroom from '../assets/flippingClassroom.jpg';
-import gameDesign from '../assets/gameDesign.jpg';
+import backend1 from '../assets/backend1.jpg';
+import blog from '../assets/blog1.jpg';
+import eCommerce from '../assets/ecommerce.jpg';
 import resume from '../assets/resume.jpg';
+import liveEvents from '../assets/liveEvents1.jpg';
 import portfolio from '../assets/portfolio.jpg';
+import newsGrid from '../assets/newsGrid1.jpg';
+import handShake from '../assets/chat1.jpg';
 
 export const Portfolio = () => {
   return (
@@ -21,41 +25,88 @@ export const Portfolio = () => {
           </a>
 
           <a
-            href='https://drive.google.com/drive/folders/1sr988AoGSg_Dpqe7LPhqjoJytonCRGsQ'
+            href='https://github.com/jonathanjacka/proshop_mern_ecommerce'
             target='_blank'
             rel='noreferrer'
           >
             <MediaCard
-              image={portfolio}
-              title='Portfolio'
-              content='Portfolio of Lesson Examples'
+              image={eCommerce}
+              title='mern-ecommerce'
+              content='E-Commerce Site: MERN Stack'
             />
           </a>
 
           <a
-            href='https://www.youtube.com/watch?v=XhJE8EpCiLY&feature=youtu.be'
+            href='https://github.com/jonathanjacka/devcamper_api'
             target='_blank'
             rel='noreferrer'
           >
             <MediaCard
-              image={flippedClassroom}
-              title='Flipped Class'
+              image={backend1}
+              title='Backend API'
               content='
-              CPS Tech Talk 2019:
-              Flipping your classroom
+              DevCamper Restful API: NodeJS/Express
               '
             />
           </a>
 
           <a
-            href='https://www.youtube.com/watch?v=bKFBEni4UJ4&feature=youtu.be'
+            href='https://github.com/jonathanjacka/devspace-blog'
             target='_blank'
             rel='noreferrer'
           >
             <MediaCard
-              image={gameDesign}
-              title='Game Design'
-              content='Class Elective Promo: Game Design with Unity'
+              image={blog}
+              title='liveEvents'
+              content='DevSpace Blog: Next.JS and TailwindCSS'
+            />
+          </a>
+
+          <a
+            href='https://github.com/jonathanjacka/live-events-frontend'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <MediaCard
+              image={liveEvents}
+              title='liveEvents'
+              content='Live Events: Next.JS and Strapi'
+            />
+          </a>
+
+          <a
+            href='https://github.com/jonathanjacka/portfolio_sass'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <MediaCard
+              image={portfolio}
+              title='liveEvents'
+              content='Portfolio Design: HTML, JavaScript, Sass'
+            />
+          </a>
+
+          <a
+            href='https://github.com/jonathanjacka/news_grid_website'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <MediaCard
+              image={newsGrid}
+              title='newsGrid'
+              content='NewsGrid Website: Study in CSS Grid'
+            />
+          </a>
+
+          <a
+            href='https://github.com/jonathanjacka/handshake_video_chat_app'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <MediaCard
+              image={handShake}
+              title='handShake'
+              content='HandShake Video Chat App: Socket.io'
             />
           </a>
         </div>
